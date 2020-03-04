314ec18f817d3b64eaf2c71ac8d55f650c1e6185
Andres Lujan <alujan@hugeinc.com>
2020-03-03 23:30:12 -0500

    WIP

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
 import os
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
 # app.config.from_object(Config)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'secret-key!'
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-from app import routes
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'secret-key!'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY__DATABASE_URI') or 'sqlite:///{}'.format(os.path.join(basedir, 'app.db'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
